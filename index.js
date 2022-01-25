5266375af620afe14caab979edd26027636f6464
Josh Wilson <joshua.wilson6289@gmail.com>
2022-01-25 11:28:32 -0600

    set up with flexbox

---
+++ index.js
@@ -0,0 +1,46 @@
+const grid = document.querySelector(".container");
+const clearBtn = document.getElementById("clear-btn")
+
+clearBtn.addEventListener("click", () => {
+  clear();
+});
+
+createRows();
+createSquares();
+hoverSquares();
+
+function createRows () {
+  for (let i=0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    grid.appendChild(row);
+  }
+}
+
+function createSquares () {
+  let rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    for (i=0; i < 16; i++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
+  });
+}
+
+function hoverSquares () {
+  let squares = document.querySelectorAll(".square")
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = "black";
+    })
+  });
+  }
+
+  function clear () {
+    let squares = document.querySelectorAll(".square")
+    squares.forEach((square) => {
+      square.style.backgroundColor = "white";
+    }) 
+
+  }
